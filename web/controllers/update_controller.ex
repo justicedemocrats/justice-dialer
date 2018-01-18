618a677684b8a0eb5215ab82cac706334b4b1b46
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-18 13:09:13 -0500

    add cosmic updating

---
+++ web/controllers/update_controller.ex
@@ -0,0 +1,13 @@
+defmodule JusticeDialer.UpdateController do
+  use JusticeDialer.Web, :controller
+  alias Phoenix.{PubSub}
+  require Logger
+
+  def cosmic(conn, _params) do
+    PubSub.broadcast(:core, "update", "update")
+
+    json(conn, %{
+      "unnecessary" => "Ben implemented webhooks! No need to visit hit this link any more, but an update just happened just in case. If it's not updating, contact Ben. Thanks!"
+    })
+  end
+end
